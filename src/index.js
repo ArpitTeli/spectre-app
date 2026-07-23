8b7fb569ee1508720b1d87552b1bee5b0f7c9ce5
ArpitTeli <arpitteli4@gmail.com>
2026-07-23 15:20:33 +0530

    v1.11.28: Version bump + fix processArmaUpdate preserving unit data across batches

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-window.__APP_VERSION = '1.11.27';
+window.__APP_VERSION = '1.11.28';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
