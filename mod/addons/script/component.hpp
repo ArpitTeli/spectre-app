1aec580988c45968c1e63085fce26dd172f64245
ArpitTeli <arpitteli4@gmail.com>
2026-07-29 16:28:58 +0530

    v1.12.18 Restore original config + remove PBO prefix + single PBO

--- mod/addons/script/component.hpp
+++
@@ -1,4 +0,0 @@
-#define PREFIX spectre
-#define COMPONENT bridge
-
-#include "\z\spectre\addons\spectre_bridge\script\macro.hpp"
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-window.__APP_VERSION = '1.12.17';
+window.__APP_VERSION = '1.12.18';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
